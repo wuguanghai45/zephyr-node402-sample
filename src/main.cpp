0980c856bc4c0294078706288a3315d244a1a8b4
Guanghai Wu <wuguanghai45@126.com>
2025-03-19 11:43:02 +0000

    Refactor motor control logic and enhance event logging
    - Simplify motor control direction logic by using a ternary operator.
    - Add logging for target reached event in node402_event_emitter function.
    - Remove unused variables and clean up main function for better readability.

--- src/main.cpp
+++ src/main.cpp
@@ -32,20 +32,20 @@ enum CanbusDef : uint8_t
     CAN_NUM = 1,
 };
 
-void node402_event_emitter(Node402Event e) {
-    LOG_INF("Got motor event: %d", e.type);
-}
-
 Node402* motor;  // Use a pointer for the motor
 int target = 100000;
 
+void node402_event_emitter(Node402Event e) {
+    if (e.type == NODE402_TARGET_REACHED) {
+        LOG_INF("Target reached");
+    }
+    LOG_INF("Got motor event: %d", e.type);
+}
+
 // Function to control motor direction
 void control_motor_direction(bool forward) {
-    if (forward) {
-        motor->set_target_position(target);
-    } else {
-        motor->set_target_position(-target);
-    }
+    motor->enable();
+    motor->set_target_position(forward ? target : -target);
     motor->start_profile_position_mode_1(true);
     motor->start_profile_position_mode_2(true, true);
 }
@@ -77,8 +77,6 @@ SHELL_CMD_REGISTER(motor, NULL, "Control motor direction", cmd_motor_control);
 
 int main(void)
 {
-    int ret;
-
     CANopenStack canopen_stack(DEVICE_DT_GET(DT_ALIAS(can1)), 1000, CAN_1);
 
     canopen_stack.init();
@@ -102,36 +100,10 @@ int main(void)
     motor->start_profile_position_mode_1(true);
     motor->start_profile_position_mode_2(true, true);
 
-    uint32_t data;
-    size_t read_size;
-    ret = canopen_stack.get_sdo(4, 0x1000, 0, (uint8_t *)&data, 4, &read_size);
-    if (!ret)
-    {
-        LOG_INF("Device type: 0x%" PRIx32, data);
-    }
-    else
-    {
-        LOG_ERR("SDO Error: %" PRIi32, ret);
-    }
-
-    if (!device_is_ready(led.port))
-    {
-        return -1;
-    }
-
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0)
-    {
-        return ret;
-    }
-    int t = 0;
     while (1)
     {
         k_msleep(SLEEP_TIME_MS);
     }
-
-    // Clean up
-    delete motor;
 
     return 0;
 }
